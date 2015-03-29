ba12caedaa00f96269cd88424111587d9d5a177b
stsfin <jonne.backhaus@gmail.com>
2015-03-29 16:54:39 +0300

    Update limelightReconfig.sh

--- limelightReconfig.sh
+++ limelightReconfig.sh
@@ -6,27 +6,27 @@ echo -e "\nDiscovering GeForce PC:s, when found you can press ctrl+c to stop the
 
 java -jar limelight.jar discover
 
-read -p "Input ip-address given above (if no IP is shown, press CTRL+C and check host connection) :`echo $'\n> '`" ip
+read -p "Input ip-address given above (if no IP is shown, press CTRL+C and check host connection) :`echo $'\n> '`" ip < /dev/tty
 
 java -jar limelight.jar pair $ip
 
-read -p "Press any key to continue after you have given the passcode to the Host PC... `echo $'\n> '`" -n1 -s
+read -p "Press any key to continue after you have given the passcode to the Host PC... `echo $'\n> '`" -n1 -s < /dev/tty
 
-read -p "Please ensure that your gamepad is connected to the PI for device selection, press any key to continue... `echo $'\n> '`" -n1 -s
+read -p "Please ensure that your gamepad is connected to the PI for device selection, press any key to continue... `echo $'\n> '`" -n1 -s < /dev/tty
 
 clear
 
 lsinput|grep -e dev.input.event -e name
 
-echo -e "\nInput device event ID-number that corresponds with your gamepad from above for keymapping \n"
-read -p "(if the gamepad is missing, press CTRL+C and reboot the PI with the game pad attached) :`echo $'\n> '`" USBID
+echo -e "\nInput device event ID-number that corresponds with your gamepad from above for keymapping \n" < /dev/tty
+read -p "(if the gamepad is missing, press CTRL+C and reboot the PI with the game pad attached) :`echo $'\n> '`" USBID < /dev/tty
 
 java -jar limelight.jar map -input /dev/input/event$USBID mapfile.map
 
 cd /home/pi/RetroPie/roms/limelight/
 
 read -p "Existing limelight start scripts made with this installer will be removed, and new ones created in their place, press anykey to continue `echo $'\n> '`" -n1 -s
-
+  < /dev/tty
 rm limelight720p60fps.sh  2> /dev/null
 rm limelight1080p30fps.sh  2> /dev/null
 rm limelight1080p60fps.sh 2> /dev/null
